a53e52824f8b5d6106e916d3a535eaa4d7bf5d45
robinbastien <info@robinbastien.com>
2016-11-20 16:36:28 -0500

    button now creates new list items

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,20 +2,43 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 
-const notes = [
-	{
-		id: uuid.v4(),
-		task: 'Gather Mushrooms'
-	},
-	{
-		id: uuid.v4(),
-		task: 'Pet Hedgehog'
-	}
-];
+export default class App extends React.Component {
 
-export default () => (
-  <div>
-    <button onClick={() => console.log('add note')}>+</button>
-    <Notes notes={notes} />
-  </div>
-);
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Gather Mushrooms'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Pet Hedgehog'
+        }
+      ]
+    };
+  }
+
+  render() {
+    const {notes} = this.state;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
+      </div>
+    )
+  }
+
+  addNote = () => {
+      this.setState({
+        notes: this.state.notes.concat([{
+          id: uuid.v4(),
+          task: 'New task'
+        }])
+      });
+  }
+
+}
